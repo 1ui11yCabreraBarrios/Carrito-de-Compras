530de43f0d25a07180bfb7455bdcb6f9a7cb7f74
luillyCabreraBarrios <4lbertcabrerab@gmail.com>
2021-03-09 18:12:27 -0600

    Usando HTML, CSS y JavaScript

---
+++ js/app.js
@@ -0,0 +1,146 @@
+// mvariables 
+const carrito  = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listasCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
+
+
+registarEventListeners();
+
+function registarEventListeners() {
+    //cuando agregas un curso presionando "agregra al carrito"
+    listasCursos.addEventListener('click', agregarCurso);
+
+
+    //Elimina cursos del carrito
+    carrito.addEventListener('click', eliminarCurso); 
+
+    // cunado el documento este listo 
+     document.addEventListener('DOMContentLoaded', () => {
+    articulosCarrito =JSON.parse(localStorage.getItem('carrito')) || [];
+    
+       carritoHTML();
+    })
+
+
+    // vaciar cursos de carrito
+    vaciarCarritoBtn.addEventListener('click', () => {
+        articulosCarrito = []; // reseteamos el arreglo 
+
+        limpiarHTML(); // Eliminamos todo el HTML
+    })
+}
+
+//Funciones 
+// Agregamos curso al Carrito
+function agregarCurso(e){
+    e.preventDefault();
+      // Se declara el curso que se va agregar al carrito
+    if(e.target.classList.contains('agregar-carrito') ){
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        //Se envia el curso seleccionado para tomar sus datos
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+//Elimina un curso del carrito
+function eliminarCurso(e){
+    if(e.target.classList.contains('borrar-curso')){
+        const cursoId = e.target.getAttribute('data-id');
+
+        //Elimina del arreglo de articuloCarrito por el data-id
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+        carritoHTML(); //Iterar sobre el carrito y mostrar su HTML
+    }
+}
+
+// Lee el contenido del HTML al que damos click y extrae la info del contenido 
+
+function leerDatosCurso(curso){
+   // console.log(curso);
+
+// crear el objetocon el contenido del curso actual 
+   const infoCurso ={
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1
+    
+}
+
+// Revisa si un elemento ya existe en el carrito
+const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+if(existe){
+    //actualizamos la cantidad
+    const cursos = articulosCarrito.map(curso => {
+        if(curso.id === infoCurso.id){
+            curso.cantidad++;
+            return curso; // retorna el objeto actuazilado 
+        }else{
+            return curso; //retorna los objetos que no son duplicados 
+        }
+
+        
+    });
+
+    articulosCarrito = [...cursos];
+
+
+
+}else{
+    //agrega elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+}
+
+
+ 
+ carritoHTML();
+}
+
+ 
+// Muestra el carrito de compras en el html
+function carritoHTML(){
+
+    //Limpiar el HTMl
+    limpiarHTML();
+
+
+    //Recorre el carrito y genera el HTML
+
+    articulosCarrito.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+
+        <td> <img src=${imagen} width="100"</td>
+        <td> ${titulo}</td>
+        <td> ${precio}</td>
+        <td> ${cantidad}</td>
+        <td> <a href="#" class="borrar-curso" data-id="${id}" > X </a> </td>
+        `;
+        //agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+
+    });
+
+    sincronizarStorage();
+}
+
+ /// Agregar los carrito actaules a localstorage
+ function sincronizarStorage(){
+    localStorage.setItem('carrito',JSON.stringify(articulosCarrito));
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML(){
+    //Forma lenta
+    //contenedorCarrito.innerHTML = '';
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+
+
+}
